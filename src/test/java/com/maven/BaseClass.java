aea6e6b11784098ed4d4c01a41b27d693f49f665
msharfi17 <mohamed.sharfi25@gmail.com>
2018-12-29 12:05:18 -0500

    initial commit

---
+++ src/test/java/com/maven/BaseClass.java
@@ -0,0 +1,16 @@
+package com.maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	@Test
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+
+
+}
