d01808fc97472995b44009a3a840fb4fc956e8d1
msharfi17 <mohamed.sharfi25@gmail.com>
2018-12-29 12:23:54 -0500

    baseclassupdate

--- src/test/java/com/maven/BaseClass.java
+++ src/test/java/com/maven/BaseClass.java
@@ -10,6 +10,8 @@ public class BaseClass {
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        
+        driver.close();
     }
 
 
